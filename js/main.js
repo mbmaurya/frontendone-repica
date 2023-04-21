61bf3fe2c91fdec5709a0dc4f3065344650d9c1d
Mukesh Maurya <mbmaurya.maurya@gmail.com>
2023-04-21 17:49:05 +0530

    add slick slider

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".client-speak-slider").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    dots: true,
+  });
+});
